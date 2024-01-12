675cb4af672ed4cc85a1d8b2062243acd1f0da08
PichaevDmitriyExpera <89770524+PichaevDmitriyExpera@users.noreply.github.com>
2024-01-12 18:37:14 +0500

    add disent error handling

--- src/main/java/guru/springframework/spring6restmvc/controllers/CustomErrorController.java
+++ src/main/java/guru/springframework/spring6restmvc/controllers/CustomErrorController.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6restmvc.controllers;
 
+import jakarta.validation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.*;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -7,10 +8,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.*;
+import java.util.stream.*;
 
 @ControllerAdvice
 public class CustomErrorController {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler
     ResponseEntity handleError(MethodArgumentNotValidException exception){
         List errorList = exception.getFieldErrors().stream()
                 .map(fieldError -> {
@@ -21,9 +23,22 @@ public class CustomErrorController {
         return ResponseEntity.badRequest().body(errorList);
     }
 
-    @ExceptionHandler(TransactionSystemException.class)
-    ResponseEntity handleJPAValidation(){
-      return ResponseEntity.badRequest().build();
+    @ExceptionHandler
+    ResponseEntity handleJPAValidation(TransactionSystemException exception){
+      ResponseEntity.BodyBuilder responseEntity = ResponseEntity.badRequest();
+      if (exception.getCause().getCause() instanceof ConstraintViolationException){
+        ConstraintViolationException ve = (ConstraintViolationException) exception.getCause().getCause();
+        List errors = ve.getConstraintViolations().stream()
+          .map(constraintViolation -> {
+            Map<String, String> errMap = new HashMap<>();
+            errMap.put(constraintViolation.getPropertyPath().toString(),
+              constraintViolation.getMessage());
+            return errMap;
+          })
+          .collect(Collectors.toList());
+        return responseEntity.body(errors);
+      }
+      return responseEntity.build();
     }
 
 
